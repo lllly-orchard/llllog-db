1da9f69ed29620dbfbfe390213b36956cf8c5b5c
Lilly Emerson <lilly@digitalorchard.dev>
2025-06-03 16:46:02 -0600

    add index unit tests
    Signed-off-by: Lilly Emerson <lilly@digitalorchard.dev>

--- src/lib.rs
+++ src/lib.rs
@@ -150,6 +150,62 @@ mod index {
             self.map.get(key)
         }
     }
+
+    #[cfg(test)]
+    mod tests {
+        use super::*;
+
+        #[test]
+        fn it_builds() {
+            SingleFileIndex::new();
+        }
+
+        #[test]
+        fn get_returns_none_when_empty() {
+            let index = SingleFileIndex::new();
+            assert_eq!(index.get("key"), None);
+        }
+
+        #[test]
+        fn set_returns_total_bytes_written() {
+            let mut index = SingleFileIndex::new();
+
+            let k1_size = 11;
+            let size = index.set("k1", k1_size);
+            assert_eq!(size, 11);
+
+            let k2_size = 12;
+            let size = index.set("k2", k2_size);
+            assert_eq!(size, k1_size + k2_size);
+        }
+
+        #[test]
+        fn get_returns_correct_offset_and_value_size() {
+            let mut index = SingleFileIndex::new();
+
+            let k1_size = 11;
+            let k1_key = "key1";
+
+            let file_size_new = 0;
+            let file_size_after_k1 = index.set(k1_key, k1_size);
+
+            let result = index.get(k1_key).unwrap();
+
+            let comma_len = ",".len();
+            let newline_len = "\n".len();
+            let key_len = k1_key.len();
+            assert_eq!(*result, (file_size_new + key_len + comma_len, k1_size - key_len - comma_len - newline_len));
+
+            let k2_size = 12;
+            let k2_key = "key2";
+            index.set(k2_key, k2_size);
+
+            let result = index.get(k2_key).unwrap();
+
+            let key_len = k2_key.len();
+            assert_eq!(*result, (file_size_after_k1 + key_len + comma_len, k2_size - key_len - comma_len - newline_len));
+        }
+    }
 }
 
 
